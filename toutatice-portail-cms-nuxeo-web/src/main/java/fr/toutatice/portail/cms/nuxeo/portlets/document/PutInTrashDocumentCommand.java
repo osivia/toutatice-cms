ce29bc335e1fcea23e7ed07eec1a5419c910f8f1
no_author <contact@osivia.com>
2014-01-16 15:19:25 +0000

    FIXES#303: changement de la commande permettant de placer un document à l'état "deleted"

--- toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/portlets/document/PutInTrashDocumentCommand.java
+++ toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/portlets/document/PutInTrashDocumentCommand.java
@@ -4,9 +4,6 @@
 package fr.toutatice.portail.cms.nuxeo.portlets.document;
 
 import org.nuxeo.ecm.automation.client.Session;
-import org.nuxeo.ecm.automation.client.adapters.DocumentService;
-import org.nuxeo.ecm.automation.client.model.DocRef;
-import org.nuxeo.ecm.automation.client.model.Document;
 
 import fr.toutatice.portail.cms.nuxeo.api.INuxeoCommand;
 
@@ -15,7 +12,7 @@ import fr.toutatice.portail.cms.nuxeo.api.INuxeoCommand;
  * @author david
  */
 public class PutInTrashDocumentCommand implements INuxeoCommand {
-
+    
     private String docId;
 
     public PutInTrashDocumentCommand(String docId) {
@@ -26,9 +23,9 @@ public class PutInTrashDocumentCommand implements INuxeoCommand {
      * @see fr.toutatice.portail.cms.nuxeo.api.INuxeoCommand#execute(org.nuxeo.ecm.automation.client.Session)
      */
     public Object execute(Session nuxeoSession) throws Exception {
-        DocumentService documentService = nuxeoSession.getAdapter(DocumentService.class);
-        DocRef docRef = Document.newRef(docId);
-        return documentService.setState(docRef, "delete");
+        
+        return nuxeoSession.newRequest("Document.PutDocumentInTrash").set("document", docId).execute();
+
     }
 
     /* (non-Javadoc)
