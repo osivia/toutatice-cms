f7bff80b78bc3a7cf2034c1114853810b2d1e332
no_author <contact@osivia.com>
2014-05-20 13:56:06 +0000

    Conersion of Json -> List<CMSItem> added

--- toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/portlets/service/ListCMSSubitemsCommand.java
+++ toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/portlets/service/ListCMSSubitemsCommand.java
@@ -16,17 +16,30 @@
  */
 package fr.toutatice.portail.cms.nuxeo.portlets.service;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 import org.apache.commons.io.IOUtils;
 import org.nuxeo.ecm.automation.client.Constants;
 import org.nuxeo.ecm.automation.client.OperationRequest;
 import org.nuxeo.ecm.automation.client.Session;
 import org.nuxeo.ecm.automation.client.model.Blob;
-import org.osivia.portal.core.constants.InternalConstants;
+import org.nuxeo.ecm.automation.client.model.Document;
+import org.nuxeo.ecm.automation.client.model.PropertyMap;
+import org.osivia.portal.core.cms.CMSException;
+import org.osivia.portal.core.cms.CMSItem;
+import org.osivia.portal.core.cms.CMSItemType;
+import org.osivia.portal.core.cms.CMSServiceCtx;
+import org.osivia.portal.core.cms.ICMSService;
 
 import fr.toutatice.portail.cms.nuxeo.api.INuxeoCommand;
-import fr.toutatice.portail.cms.nuxeo.api.NuxeoQueryFilter;
+import fr.toutatice.portail.cms.nuxeo.api.NuxeoController;
 
 /**
  * List CMS sub-items command.
@@ -40,6 +53,8 @@ public class ListCMSSubitemsCommand implements INuxeoCommand {
     private final String parentId;
     /** Live content indicator. */
     private final boolean liveContent;
+    /** CMS Context */
+    private CMSServiceCtx cmsContext;
 
 
     /**
@@ -48,8 +63,9 @@ public class ListCMSSubitemsCommand implements INuxeoCommand {
      * @param parentId current parent identifier
      * @param liveContent live content indicator
      */
-    public ListCMSSubitemsCommand(String parentId, boolean liveContent) {
+    public ListCMSSubitemsCommand(CMSServiceCtx cmsContext, String parentId, boolean liveContent) {
         super();
+        this.cmsContext = cmsContext;
         this.parentId = parentId;
         this.liveContent = liveContent;
     }
@@ -58,7 +74,7 @@ public class ListCMSSubitemsCommand implements INuxeoCommand {
     /**
      * {@inheritDoc}
      */
-    public Object execute(Session nuxeoSession) throws Exception {
+    public List<CMSItem> execute(Session nuxeoSession) throws Exception {
         // Fetch live tree with publishing infos
         OperationRequest request = nuxeoSession.newRequest("Fetch.PublishingStatusChildren");
         request.set("documentId", this.parentId);
@@ -67,7 +83,47 @@ public class ListCMSSubitemsCommand implements INuxeoCommand {
         Blob binariesPublishingInfos = (Blob) request.execute();
 
         String publishingInfos = IOUtils.toString(binariesPublishingInfos.getStream(), "UTF-8");
-        return JSONArray.fromObject(publishingInfos);
+        return convertPublishingInfos(JSONArray.fromObject(publishingInfos));
+    }
+    
+    /**
+     * Convert the JsonArray returned by command to list of exposed CMSItems.
+     * @param publishingInfos
+     * @return
+     * @throws CMSException
+     * @throws UnsupportedEncodingException 
+     */
+    private List<CMSItem> convertPublishingInfos(JSONArray publishingInfos) throws CMSException, UnsupportedEncodingException{
+        List<CMSItem> cmsItems = new ArrayList<CMSItem>(publishingInfos.size());
+        Iterator<?> documentsIterator = publishingInfos.iterator();
+        while (documentsIterator.hasNext()) {
+            JSONObject documentWithPublishingStatus = (JSONObject) documentsIterator.next();
+
+            String documentId = (String) documentWithPublishingStatus.get("docId");
+            String documentPath = URLDecoder.decode((String) documentWithPublishingStatus.get("docPath"), "UTF-8");
+            String documentType = (String) documentWithPublishingStatus.get("docType");
+
+            String documentTitle = URLDecoder.decode((String) documentWithPublishingStatus.get("docTitle"), "UTF-8");
+            PropertyMap nxProperties = new PropertyMap();
+            nxProperties.set("dc:title", documentTitle);
+
+            Document poorDocument = new Document(documentId, documentType, null, null, documentPath, null, null, null, null, null, nxProperties, null);
+            
+            CMSService cmsService = (CMSService) NuxeoController.getCMSService();
+            CMSItem cmsItem = cmsService.createItem(this.cmsContext, poorDocument.getPath(), poorDocument.getTitle(), poorDocument);
+
+            boolean isPublished = documentWithPublishingStatus.getBoolean("isPublished");
+            boolean isLiveModifiedFromProxy = documentWithPublishingStatus.getBoolean("isLiveModifiedFromProxy");
+            cmsItem.setPublished(Boolean.valueOf(isPublished));
+            cmsItem.setBeingModified(Boolean.valueOf(isLiveModifiedFromProxy));
+
+            boolean isFolderish = documentWithPublishingStatus.getBoolean("isFolderish");
+            CMSItemType cmsItemType = new CMSItemType(documentType, isFolderish, false, false, false, false, null, null);
+            cmsItem.setType(cmsItemType);
+            
+            cmsItems.add(cmsItem);
+        }
+        return cmsItems;
     }
 
 
