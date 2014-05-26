aa97781f1f588aa2d17524a7b83e2f1b9a75092f
no_author <contact@osivia.com>
2014-05-26 14:36:08 +0000

    Fix to be able to attach blob to Post

--- toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/portlets/document/comments/AddCommentCommand.java
+++ toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/portlets/document/comments/AddCommentCommand.java
@@ -1,24 +1,22 @@
 /*
  * (C) Copyright 2014 Académie de Rennes (http://www.ac-rennes.fr/), OSIVIA (http://www.osivia.com) and others.
- *
+ * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 2.1 which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/lgpl-2.1.html
- *
+ * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- *
- *
- *    
  */
 package fr.toutatice.portail.cms.nuxeo.portlets.document.comments;
 
 import java.io.File;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
 import org.nuxeo.ecm.automation.client.OperationRequest;
 import org.nuxeo.ecm.automation.client.Session;
 import org.nuxeo.ecm.automation.client.adapters.DocumentService;
@@ -52,12 +50,19 @@ public class AddCommentCommand implements INuxeoCommand {
         request.set("title", commentTitle);
         if (hasFile) {
             request.set("fileName", file.getName());
+        } else {
+            /* Nuxeo operations need no null input */
+            file = File.createTempFile("tmp_com", ".tmp");
         }
-        Blob commentIdBinary = (Blob) request.execute();
-        if (hasFile) {
-            String commentId = IOUtils.toString(commentIdBinary.getStream(), "UTF-8");
-            setBlob(nuxeoSession, commentId, file);
+
+        request.setInput(new FileBlob(file));
+        request.execute();
+
+        /* To avoid temporary file persistence */
+        if (!hasFile) {
+            file.delete();
         }
+
         return document;
     }
 
