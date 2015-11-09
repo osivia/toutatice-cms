81ee510beeb7b0d8e295960f746dea307f782d90
no_author <contact@osivia.com>
2015-11-09 15:00:58 +0000

    Correction d'une anomalie concernant l'outil de réorganisation des pages web.

--- toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/portlets/document/helpers/DocumentHelper.java
+++ toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/portlets/document/helpers/DocumentHelper.java
@@ -1,11 +1,11 @@
 /*
  * (C) Copyright 2014 Académie de Rennes (http://www.ac-rennes.fr/), OSIVIA (http://www.osivia.com) and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 2.1 which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/lgpl-2.1.html
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -15,7 +15,8 @@ package fr.toutatice.portail.cms.nuxeo.portlets.document.helpers;
 
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
+import javax.portlet.PortletRequest;
+
 import org.nuxeo.ecm.automation.client.model.Document;
 import org.nuxeo.ecm.automation.client.model.PropertyList;
 import org.osivia.portal.api.contribution.IContributionService.EditionState;
@@ -26,19 +27,19 @@ import org.osivia.portal.core.cms.CMSServiceCtx;
 /**
  * Utility class to get informations on Document object
  * (Nuxeo automation client).
- * 
+ *
  * @author David Chevrier.
  *
  */
 public class DocumentHelper {
-    
+
     /**
      * Private constructor.
      */
     private DocumentHelper(){
-        
+
     }
-    
+
     /**
      * @param document studied document
      * @return true if document is a folder
@@ -47,7 +48,7 @@ public class DocumentHelper {
         boolean isFolder = false;
 
         PropertyList facetsProp = document.getFacets();
-        if (facetsProp != null && !facetsProp.isEmpty()) {
+        if ((facetsProp != null) && !facetsProp.isEmpty()) {
             List<Object> facets = facetsProp.list();
             isFolder = facets.contains(DocumentConstants.FOLDERISH_FACET);
         }
@@ -55,7 +56,7 @@ public class DocumentHelper {
         return isFolder;
     }
 
-    
+
     /**
      * Checks if current document is a remote proxy.
      *
@@ -82,7 +83,7 @@ public class DocumentHelper {
 
         return false;
     }
-    
+
     /**
      * Controls if live mode is associated with the current document.
      *
@@ -93,23 +94,29 @@ public class DocumentHelper {
     public static boolean isInLiveMode(CMSServiceCtx cmsCtx, CMSPublicationInfos pubInfos) {
         boolean liveMode = false;
 
-        EditionState curState = (EditionState) cmsCtx.getRequest().getAttribute("osivia.editionState");
-        if ((curState != null) && curState.getContributionMode().equals(EditionState.CONTRIBUTION_MODE_EDITION)) {
-            if (curState.getDocPath().equals(pubInfos.getDocumentPath())) {
-                liveMode = true;
+        // Portlet request
+        PortletRequest request = cmsCtx.getRequest();
+        if (request != null) {
+            // Current edition state
+            EditionState curState = (EditionState) request.getAttribute("osivia.editionState");
+            if ((curState != null) && curState.getContributionMode().equals(EditionState.CONTRIBUTION_MODE_EDITION)) {
+                if (curState.getDocPath().equals(pubInfos.getDocumentPath())) {
+                    liveMode = true;
+                }
             }
         }
         return liveMode;
     }
-    
+
     /**
      * @param path
      * @return navigation path.
      */
     public static String  computeNavPath(String path){
         String result = path;
-        if( path.endsWith(".proxy"))
+        if( path.endsWith(".proxy")) {
             result = result.substring(0, result.length() - 6);
+        }
         return result;
     }
 }
