26f23c5748e342b1b8c0ab4f8361d4f16d9e1215
no_author <contact@osivia.com>
2016-02-25 14:17:19 +0000

    Suppression de la création d'objets à la racine d'un workspace.

--- toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/portlets/customizer/DefaultCMSCustomizer.java
+++ toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/portlets/customizer/DefaultCMSCustomizer.java
@@ -1378,7 +1378,8 @@ public class DefaultCMSCustomizer implements INuxeoCustomizer {
         List<DocumentType> defaultTypes = new ArrayList<DocumentType>();
 
         // Workspace
-        defaultTypes.add(new DocumentType("Workspace", true, false, false, true, false, false, Arrays.asList("Folder"), "/default/templates/workspace", "glyphicons glyphicons-wallet", true));
+        defaultTypes.add(new DocumentType("Workspace", true, false, false, true, false, false, new ArrayList<String>(0), "/default/templates/workspace",
+                "glyphicons glyphicons-wallet", true));
         // Portal site
         defaultTypes.add(new DocumentType("PortalSite", true, false, false, true, true, true, Arrays.asList("File", "Audio", "Video", "PortalPage",
                 "ContextualLink"), null, "glyphicons glyphicons-global", true));
