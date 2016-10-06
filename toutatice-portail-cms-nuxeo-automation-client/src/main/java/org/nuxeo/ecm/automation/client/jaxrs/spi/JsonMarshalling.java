9c7d30e53e89088e87f2e411d7a78ac1be170bd3
no_author <contact@osivia.com>
2016-10-06 15:08:47 +0000

    [RM #8202] la liste des mots clés neprésente as que les mots clés des espaces collaboratifs - ajout du RawESMarshaller pour parser des reponses elasticsearch brute

--- toutatice-portail-cms-nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/jaxrs/spi/JsonMarshalling.java
+++ toutatice-portail-cms-nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/jaxrs/spi/JsonMarshalling.java
@@ -1,13 +1,11 @@
 /*
  * Copyright (c) 2006-2011 Nuxeo SA (http://nuxeo.com/) and others.
- *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
  * Contributors:
- *     bstefanescu
+ * bstefanescu
  */
 package org.nuxeo.ecm.automation.client.jaxrs.spi;
 
@@ -17,7 +15,6 @@ import java.lang.reflect.Type;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonGenerator;
 import org.codehaus.jackson.JsonNode;
@@ -46,6 +43,7 @@ import org.nuxeo.ecm.automation.client.jaxrs.spi.marshallers.DocumentsMarshaller
 import org.nuxeo.ecm.automation.client.jaxrs.spi.marshallers.EsMarshaller;
 import org.nuxeo.ecm.automation.client.jaxrs.spi.marshallers.ExceptionMarshaller;
 import org.nuxeo.ecm.automation.client.jaxrs.spi.marshallers.LoginMarshaller;
+import org.nuxeo.ecm.automation.client.jaxrs.spi.marshallers.RawEsMarshaller;
 import org.nuxeo.ecm.automation.client.jaxrs.util.JsonOperationMarshaller;
 import org.nuxeo.ecm.automation.client.model.OperationDocumentation;
 import org.nuxeo.ecm.automation.client.model.OperationInput;
@@ -56,257 +54,249 @@ import org.nuxeo.ecm.automation.client.model.OperationRegistry;
  */
 public class JsonMarshalling {
 
-    /**
-     * 
-     * @author matic
-     * @since 5.5
-     */
-    public static class ThowrableTypeModifier extends TypeModifier {
-        @Override
-        public JavaType modifyType(JavaType type, Type jdkType,
-                TypeBindings context, TypeFactory typeFactory) {
-            Class<?> raw = type.getRawClass();
-            if (raw.isAssignableFrom(Throwable.class)) {
-                return typeFactory.constructType(RemoteThrowable.class);
-            }
-            return type;
-        }
-    }
+	/**
+	 * @author matic
+	 * @since 5.5
+	 */
+	public static class ThowrableTypeModifier extends TypeModifier {
+		@Override
+		public JavaType modifyType(JavaType type, Type jdkType,
+				TypeBindings context, TypeFactory typeFactory) {
+			final Class<?> raw = type.getRawClass();
+			if (raw.isAssignableFrom(Throwable.class)) {
+				return typeFactory.constructType(RemoteThrowable.class);
+			}
+			return type;
+		}
+	}
 
-    public static class RemoteThrowable extends Throwable {
+	public static class RemoteThrowable extends Throwable {
 
-        private static final long serialVersionUID = 1L;
+		private static final long serialVersionUID = 1L;
 
-        protected RemoteThrowable(String message) {
-            super(message);
-        }
+		protected RemoteThrowable(String message) {
+			super(message);
+		}
 
-        protected final HashMap<String, JsonNode> otherNodes = new HashMap<String, JsonNode>();
+		protected final HashMap<String, JsonNode> otherNodes = new HashMap<String, JsonNode>();
 
-        public Map<String,JsonNode> getOtherNodes() {
-            return Collections.unmodifiableMap(otherNodes);
-        }
-    }
+		public Map<String, JsonNode> getOtherNodes() {
+			return Collections.unmodifiableMap(otherNodes);
+		}
+	}
 
-    @JsonCachable(false)
-    public static class ThrowableDeserializer extends
-            org.codehaus.jackson.map.deser.ThrowableDeserializer {
+	@JsonCachable(false)
+	public static class ThrowableDeserializer extends
+			org.codehaus.jackson.map.deser.ThrowableDeserializer {
 
-        protected HashMap<String, JsonNode> otherNodes = new HashMap<String, JsonNode>();
+		protected HashMap<String, JsonNode> otherNodes = new HashMap<String, JsonNode>();
 
-        public ThrowableDeserializer(BeanDeserializer src) {
-            super(src);
-        }
+		public ThrowableDeserializer(BeanDeserializer src) {
+			super(src);
+		}
 
-        @Override
-        public Object deserializeFromObject(JsonParser jp,
-                DeserializationContext ctxt) throws IOException,
-                JsonProcessingException {
+		@Override
+		public Object deserializeFromObject(JsonParser jp,
+				DeserializationContext ctxt) throws IOException,
+				JsonProcessingException {
 
-            RemoteThrowable t = (RemoteThrowable) super.deserializeFromObject(
-                    jp, ctxt);
-            t.otherNodes.putAll(otherNodes);
-            return t;
-        }
-    }
+			final RemoteThrowable t = (RemoteThrowable) super.deserializeFromObject(jp, ctxt);
+			t.otherNodes.putAll(otherNodes);
+			return t;
+		}
+	}
 
-    private JsonMarshalling() {
-    }
+	private JsonMarshalling() {
+	}
 
-    protected static JsonFactory factory = newJsonFactory();
+	protected static JsonFactory factory = newJsonFactory();
 
-    protected static final HashMap<String, JsonMarshaller<?>> marshallersByType = new HashMap<String, JsonMarshaller<?>>();
+	protected static final HashMap<String, JsonMarshaller<?>> marshallersByType = new HashMap<String, JsonMarshaller<?>>();
 
-    protected static final HashMap<Class<?>, JsonMarshaller<?>> marshallersByJavaType = new HashMap<Class<?>, JsonMarshaller<?>>();
+	protected static final HashMap<Class<?>, JsonMarshaller<?>> marshallersByJavaType = new HashMap<Class<?>, JsonMarshaller<?>>();
 
-    public static JsonFactory getFactory() {
-        return factory;
-    }
+	public static JsonFactory getFactory() {
+		return factory;
+	}
 
-    public static JsonFactory newJsonFactory() {
-        JsonFactory jf = new JsonFactory();
-        ObjectMapper oc = new ObjectMapper(jf);
-        final TypeFactory typeFactoryWithModifier = oc.getTypeFactory().withModifier(
-                new ThowrableTypeModifier());
-        oc.setTypeFactory(typeFactoryWithModifier);
-        oc.getDeserializationConfig().addHandler(
-                new DeserializationProblemHandler() {
-                    @Override
-                    public boolean handleUnknownProperty(
-                            DeserializationContext ctxt,
-                            JsonDeserializer<?> deserializer,
-                            Object beanOrClass, String propertyName)
-                            throws IOException, JsonProcessingException {
-                        if (deserializer instanceof ThrowableDeserializer) {
-                            JsonParser jp = ctxt.getParser();
-                            JsonNode propertyNode = jp.readValueAsTree();
-                            ((ThrowableDeserializer) deserializer).otherNodes.put(
-                                    propertyName, propertyNode);
-                            return true;
-                        }
-                        return false;
-                    }
-                });
-        final SimpleModule module = new SimpleModule("automation",
-                Version.unknownVersion()) {
+	public static JsonFactory newJsonFactory() {
+		final JsonFactory jf = new JsonFactory();
+		final ObjectMapper oc = new ObjectMapper(jf);
+		final TypeFactory typeFactoryWithModifier = oc.getTypeFactory().withModifier(new ThowrableTypeModifier());
+		oc.setTypeFactory(typeFactoryWithModifier);
+		oc.getDeserializationConfig().addHandler(new DeserializationProblemHandler() {
+			@Override
+			public boolean handleUnknownProperty(
+					DeserializationContext ctxt,
+					JsonDeserializer<?> deserializer,
+					Object beanOrClass, String propertyName)
+					throws IOException, JsonProcessingException {
+				if (deserializer instanceof ThrowableDeserializer) {
+					final JsonParser jp = ctxt.getParser();
+					final JsonNode propertyNode = jp.readValueAsTree();
+					((ThrowableDeserializer) deserializer).otherNodes.put(propertyName, propertyNode);
+					return true;
+				}
+				return false;
+			}
+		});
+		final SimpleModule module = new SimpleModule("automation", Version.unknownVersion()) {
 
-            @Override
-            public void setupModule(SetupContext context) {
-                super.setupModule(context);
+			@Override
+			public void setupModule(SetupContext context) {
+				super.setupModule(context);
 
-                context.addBeanDeserializerModifier(new BeanDeserializerModifier() {
+				context.addBeanDeserializerModifier(new BeanDeserializerModifier() {
 
-                    @Override
-                    public JsonDeserializer<?> modifyDeserializer(
-                            DeserializationConfig config,
-                            BasicBeanDescription beanDesc,
-                            JsonDeserializer<?> deserializer) {
-                        if (!Throwable.class.isAssignableFrom(beanDesc.getBeanClass())) {
-                            return super.modifyDeserializer(config, beanDesc,
-                                    deserializer);
-                        }
-                        return new ThrowableDeserializer(
-                                (BeanDeserializer) deserializer);
-                    }
-                });
-            }
-        };
-        oc.registerModule(module);
-        jf.setCodec(oc);
-        return jf;
-    }
+					@Override
+					public JsonDeserializer<?> modifyDeserializer(
+							DeserializationConfig config,
+							BasicBeanDescription beanDesc,
+							JsonDeserializer<?> deserializer) {
+						if (!Throwable.class.isAssignableFrom(beanDesc.getBeanClass())) {
+							return super.modifyDeserializer(config, beanDesc, deserializer);
+						}
+						return new ThrowableDeserializer((BeanDeserializer) deserializer);
+					}
+				});
+			}
+		};
+		oc.registerModule(module);
+		jf.setCodec(oc);
+		return jf;
+	}
 
-    static {
-        addMarshaller(new DocumentMarshaller());
-        addMarshaller(new DocumentsMarshaller());
-        addMarshaller(new ExceptionMarshaller());
-        addMarshaller(new LoginMarshaller());
-        addMarshaller(new EsMarshaller());
-    }
+	static {
+		addMarshaller(new DocumentMarshaller());
+		addMarshaller(new DocumentsMarshaller());
+		addMarshaller(new ExceptionMarshaller());
+		addMarshaller(new LoginMarshaller());
+		addMarshaller(new EsMarshaller());
+		addMarshaller(new RawEsMarshaller());
+	}
 
-    public static void addMarshaller(JsonMarshaller<?> marshaller) {
-        marshallersByType.put(marshaller.getType(), marshaller);
-        marshallersByJavaType.put(marshaller.getJavaType(), marshaller);
-    }
+	public static void addMarshaller(JsonMarshaller<?> marshaller) {
+		marshallersByType.put(marshaller.getType(), marshaller);
+		marshallersByJavaType.put(marshaller.getJavaType(), marshaller);
+	}
 
-    @SuppressWarnings("unchecked")
-    public static <T> JsonMarshaller<T> getMarshaller(String type) {
-        return (JsonMarshaller<T>) marshallersByType.get(type);
-    }
+	@SuppressWarnings("unchecked")
+	public static <T> JsonMarshaller<T> getMarshaller(String type) {
+		return (JsonMarshaller<T>) marshallersByType.get(type);
+	}
 
-    @SuppressWarnings("unchecked")
-    public static <T> JsonMarshaller<T> getMarshaller(Class<T> clazz) {
-        return (JsonMarshaller<T>) marshallersByJavaType.get(clazz);
-    }
+	@SuppressWarnings("unchecked")
+	public static <T> JsonMarshaller<T> getMarshaller(Class<T> clazz) {
+		return (JsonMarshaller<T>) marshallersByJavaType.get(clazz);
+	}
 
-    public static OperationRegistry readRegistry(String content)
-            throws Exception {
-        HashMap<String, OperationDocumentation> ops = new HashMap<String, OperationDocumentation>();
-        HashMap<String, OperationDocumentation> chains = new HashMap<String, OperationDocumentation>();
-        HashMap<String, String> paths = new HashMap<String, String>();
+	public static OperationRegistry readRegistry(String content)
+			throws Exception {
+		final HashMap<String, OperationDocumentation> ops = new HashMap<String, OperationDocumentation>();
+		final HashMap<String, OperationDocumentation> chains = new HashMap<String, OperationDocumentation>();
+		final HashMap<String, String> paths = new HashMap<String, String>();
 
-        JsonParser jp = factory.createJsonParser(content);
-        jp.nextToken(); // start_obj
-        JsonToken tok = jp.nextToken();
-        while (tok != JsonToken.END_OBJECT) {
-            String key = jp.getCurrentName();
-            if ("operations".equals(key)) {
-                readOperations(jp, ops);
-            } else if ("chains".equals(key)) {
-                readChains(jp, chains);
-            } else if ("paths".equals(key)) {
-                readPaths(jp, paths);
-            }
-            tok = jp.nextToken();
-        }
-        return new OperationRegistry(paths, ops, chains);
-    }
+		final JsonParser jp = factory.createJsonParser(content);
+		jp.nextToken(); // start_obj
+		JsonToken tok = jp.nextToken();
+		while (tok != JsonToken.END_OBJECT) {
+			final String key = jp.getCurrentName();
+			if ("operations".equals(key)) {
+				readOperations(jp, ops);
+			} else if ("chains".equals(key)) {
+				readChains(jp, chains);
+			} else if ("paths".equals(key)) {
+				readPaths(jp, paths);
+			}
+			tok = jp.nextToken();
+		}
+		return new OperationRegistry(paths, ops, chains);
+	}
 
-    private static void readOperations(JsonParser jp,
-            Map<String, OperationDocumentation> ops) throws Exception {
-        jp.nextToken(); // skip [
-        JsonToken tok = jp.nextToken();
-        while (tok != JsonToken.END_ARRAY) {
-            OperationDocumentation op = JsonOperationMarshaller.read(jp);
-            ops.put(op.id, op);
-            tok = jp.nextToken();
-        }
-    }
+	private static void readOperations(JsonParser jp,
+			Map<String, OperationDocumentation> ops) throws Exception {
+		jp.nextToken(); // skip [
+		JsonToken tok = jp.nextToken();
+		while (tok != JsonToken.END_ARRAY) {
+			final OperationDocumentation op = JsonOperationMarshaller.read(jp);
+			ops.put(op.id, op);
+			tok = jp.nextToken();
+		}
+	}
 
-    private static void readChains(JsonParser jp,
-            Map<String, OperationDocumentation> chains) throws Exception {
-        jp.nextToken(); // skip [
-        JsonToken tok = jp.nextToken();
-        while (tok != JsonToken.END_ARRAY) {
-            OperationDocumentation op = JsonOperationMarshaller.read(jp);
-            chains.put(op.id, op);
-            tok = jp.nextToken();
-        }
-    }
+	private static void readChains(JsonParser jp,
+			Map<String, OperationDocumentation> chains) throws Exception {
+		jp.nextToken(); // skip [
+		JsonToken tok = jp.nextToken();
+		while (tok != JsonToken.END_ARRAY) {
+			final OperationDocumentation op = JsonOperationMarshaller.read(jp);
+			chains.put(op.id, op);
+			tok = jp.nextToken();
+		}
+	}
 
-    private static void readPaths(JsonParser jp, Map<String, String> paths)
-            throws Exception {
-        jp.nextToken(); // skip {
-        JsonToken tok = jp.nextToken();
-        while (tok != JsonToken.END_OBJECT) {
-            jp.nextToken();
-            paths.put(jp.getCurrentName(), jp.getText());
-            tok = jp.nextToken();
-        }
-    }
+	private static void readPaths(JsonParser jp, Map<String, String> paths)
+			throws Exception {
+		jp.nextToken(); // skip {
+		JsonToken tok = jp.nextToken();
+		while (tok != JsonToken.END_OBJECT) {
+			jp.nextToken();
+			paths.put(jp.getCurrentName(), jp.getText());
+			tok = jp.nextToken();
+		}
+	}
 
-    public static Object readEntity(String content) throws Exception {
-        if (content.length() == 0) { // void response
-            return null;
-        }
-        JsonParser jp = factory.createJsonParser(content);
-        jp.nextToken(); // will return JsonToken.START_OBJECT (verify?)
-        jp.nextToken();
-        if (!Constants.KEY_ENTITY_TYPE.equals(jp.getText())) {
-            throw new RuntimeException(
-                    "unuspported respone type. No entity-type key found at top of the object");
-        }
-        jp.nextToken();
-        String etype = jp.getText();
-        JsonMarshaller<?> jm = marshallersByType.get(etype);
-        if (jm == null) {
-            throw new IllegalArgumentException("no marshaller for " + etype);
-        }
-        return jm.read(jp);
-    }
+	public static Object readEntity(String content) throws Exception {
+		if (content.length() == 0) { // void response
+			return null;
+		}
+		final JsonParser jp = factory.createJsonParser(content);
+		jp.nextToken(); // will return JsonToken.START_OBJECT (verify?)
+		jp.nextToken();
+		if (!Constants.KEY_ENTITY_TYPE.equals(jp.getText())) {
+			throw new RuntimeException("unuspported respone type. No entity-type key found at top of the object");
+		}
+		jp.nextToken();
+		final String etype = jp.getText();
+		final JsonMarshaller<?> jm = marshallersByType.get(etype);
+		if (jm == null) {
+			throw new IllegalArgumentException("no marshaller for " + etype);
+		}
+		return jm.read(jp);
+	}
 
-    public static String writeRequest(OperationRequest req) throws Exception {
-        StringWriter writer = new StringWriter();
-        OperationInput input = req.getInput();
-        JsonGenerator jg = factory.createJsonGenerator(writer);
-        jg.writeStartObject();
-        if (input != null && !input.isBinary()) {
-            String ref = input.getInputRef();
-            if (ref != null) {
-                jg.writeStringField("input", ref);
-            }
-        }
-        jg.writeObjectFieldStart("params");
-        writeMap(jg, req.getParameters());
-        jg.writeEndObject();
-        jg.writeObjectFieldStart("context");
-        writeMap(jg, req.getContextParameters());
-        jg.writeEndObject();
-        jg.writeEndObject();
-        jg.close();
-        return writer.toString();
-    }
+	public static String writeRequest(OperationRequest req) throws Exception {
+		final StringWriter writer = new StringWriter();
+		final OperationInput input = req.getInput();
+		final JsonGenerator jg = factory.createJsonGenerator(writer);
+		jg.writeStartObject();
+		if (input != null && !input.isBinary()) {
+			final String ref = input.getInputRef();
+			if (ref != null) {
+				jg.writeStringField("input", ref);
+			}
+		}
+		jg.writeObjectFieldStart("params");
+		writeMap(jg, req.getParameters());
+		jg.writeEndObject();
+		jg.writeObjectFieldStart("context");
+		writeMap(jg, req.getContextParameters());
+		jg.writeEndObject();
+		jg.writeEndObject();
+		jg.close();
+		return writer.toString();
+	}
 
-    public static void writeMap(JsonGenerator jg, Map<String, Object> map)
-            throws Exception {
-        for (Map.Entry<String, Object> entry : map.entrySet()) {
-            Object obj = entry.getValue();
-            if (obj.getClass() == String.class) {
-                jg.writeStringField(entry.getKey(), (String) entry.getValue());
-            } else {
-                throw new UnsupportedOperationException("Not yet implemented"); // TODO
-            }
-        }
-    }
+	public static void writeMap(JsonGenerator jg, Map<String, Object> map)
+			throws Exception {
+		for (final Map.Entry<String, Object> entry : map.entrySet()) {
+			final Object obj = entry.getValue();
+			if (obj.getClass() == String.class) {
+				jg.writeStringField(entry.getKey(), (String) entry.getValue());
+			} else {
+				throw new UnsupportedOperationException("Not yet implemented"); // TODO
+			}
+		}
+	}
 
 }
