6645f6604e6d836582968127bd59929576da0773
no_author <contact@osivia.com>
2017-01-26 09:43:04 +0000

    Refs #1419: ACLs of created workspace

--- toutatice-portail-cms-nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/model/DocumentPermissions.java
+++ toutatice-portail-cms-nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/model/DocumentPermissions.java
@@ -3,7 +3,12 @@
  */
 package org.nuxeo.ecm.automation.client.model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 
 /**
@@ -12,36 +17,97 @@ import java.util.Map;
  * @author david
  *
  */
-public class DocumentPermissions extends PropertyMap {
+public class DocumentPermissions {
 
-    private static final long serialVersionUID = -668727788004070858L;
+    /** Specific map. */
+    private Map<String, List<String>> aces;
 
     /**
      * 
      */
     public DocumentPermissions() {
-        super();
+        this.aces = new LinkedHashMap<String, List<String>>();
     }
 
     /**
-     * @param props
+     * @param aces
      */
-    public DocumentPermissions(PropertyMap props) {
-        super(props);
-    }
-
-    /**
-     * @param map
-     */
-    public DocumentPermissions(Map<String, Object> map) {
-        super(map);
+    public DocumentPermissions(Map<String, List<String>> aces) {
+        this.aces = aces;
     }
 
     /**
      * @param size
      */
     public DocumentPermissions(int size) {
-        super(size);
+        this.aces = new LinkedHashMap<String, List<String>>(size);
+    }
+
+    public void setPermission(String userName, String permission) {
+        if (this.aces.containsKey(userName)) {
+            List<String> permissions = this.aces.get(userName);
+            if (permissions != null) {
+                permissions.add(permission);
+            }
+        } else {
+            ArrayList<String> permissions = new ArrayList<String>();
+            permissions.add(permission);
+            this.aces.put(userName, permissions);
+        }
+    }
+
+    /**
+     * @param userName
+     * @param permissions
+     */
+    public void setPermissions(String userName, List<String> permissions) {
+        this.aces.put(userName, permissions);
+    }
+
+    /**
+     * Getter for permissions.
+     * 
+     * @param userName
+     * @return permissions
+     */
+    public List<String> getPermissions(String userName) {
+        return this.aces.get(userName);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+
+        for (final Entry<String, List<String>> ace : this.aces.entrySet()) {
+            builder.append(ace.getKey()).append("=");
+
+            final List<String> permissions = ace.getValue();
+            if (permissions != null) {
+                PropertyList propL = new PropertyList();
+                propL.getList().addAll(permissions);
+                builder.append(DocumentPermissions.toString(propL));
+            }
+
+            builder.append("\n ");
+        }
+
+        return builder.toString();
+    }
+    
+    // FIXME: to remove later: cf NuxeoJsonMapper#toString(final PropertyList list)
+    public static String toString(final PropertyList list) {
+        final StringBuilder builder = new StringBuilder("[");
+
+        final Iterator<Object> iter = list.list().iterator();
+        while (iter.hasNext()) {
+            builder.append((String) iter.next());
+            if (iter.hasNext()) {
+                builder.append(",");
+            }
+        }
+
+        builder.append("]");
+        return builder.toString();
     }
 
 }
