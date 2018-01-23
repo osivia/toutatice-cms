73165778ce54d5822f6d2faa50accf6be7e1c8f7
no_author <contact@osivia.com>
2018-01-23 17:59:16 +0000

    Change parameter name for Add.Permission

--- toutatice-portail-cms-nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/adapters/DocumentService.java
+++ toutatice-portail-cms-nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/adapters/DocumentService.java
@@ -7,7 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     bstefanescu
+ * bstefanescu
  */
 package org.nuxeo.ecm.automation.client.adapters;
 
@@ -37,7 +37,7 @@ public class DocumentService {
     public static final String FetchDocument = "Document.Fetch";
 
     public static final String CreateDocument = "Document.TTCCreate";
-    
+
     public static final String SaveDocument = "Document.Save";
 
     public static final String DeleteDocument = "Document.Delete";
@@ -91,11 +91,11 @@ public class DocumentService {
     public static final String GetBlobs = "Blob.GetList";
 
     public static final String CreateVersion = "Document.CreateVersion";
-    
+
     public static final String GetVersions = "Document.GetVersions";
 
     public static final String FireEvent = "Notification.SendEvent";
-    
+
     // The following are not yet implemented
 
     public static final String CheckOut = "Document.CheckOut";
@@ -123,8 +123,7 @@ public class DocumentService {
     }
 
     public Document getDocument(DocRef ref, String schemas) throws Exception {
-        OperationRequest req = this.session.newRequest(FetchDocument).set("value",
-                ref);
+        OperationRequest req = this.session.newRequest(FetchDocument).set("value", ref);
         if (schemas != null) {
             req.setHeader(Constants.HEADER_NX_SCHEMAS, schemas);
         }
@@ -143,10 +142,8 @@ public class DocumentService {
         return this.createDocument(parent, type, name, properties, false);
     }
 
-    public Document createDocument(DocRef parent, String type, String name,
-            PropertyMap properties, boolean synchronizedIndexing) throws Exception {
-        OperationRequest req = this.session.newRequest(CreateDocument).setInput(
-                parent).set("type", type).set("name", name);
+    public Document createDocument(DocRef parent, String type, String name, PropertyMap properties, boolean synchronizedIndexing) throws Exception {
+        OperationRequest req = this.session.newRequest(CreateDocument).setInput(parent).set("type", type).set("name", name);
         if ((properties != null) && !properties.isEmpty()) {
             properties = this.protectProperties(properties);
             req.set("properties", properties);
@@ -158,7 +155,7 @@ public class DocumentService {
 
         return (Document) req.execute();
     }
-    
+
     public Document save(DocRef doc) throws Exception {
         return (Document) this.session.newRequest(SaveDocument).setInput(doc).execute();
     }
@@ -196,13 +193,11 @@ public class DocumentService {
     }
 
     public Documents getChildren(DocRef docRef) throws Exception {
-        return (Documents) this.session.newRequest(GetDocumentChildren).setInput(
-                docRef).execute();
+        return (Documents) this.session.newRequest(GetDocumentChildren).setInput(docRef).execute();
     }
 
     public Document getChild(DocRef docRef, String name) throws Exception {
-        return (Document) this.session.newRequest(GetDocumentChild).setInput(
-                docRef).set("name", name).execute();
+        return (Document) this.session.newRequest(GetDocumentChild).setInput(docRef).set("name", name).execute();
     }
 
     public Document getParent(DocRef docRef) throws Exception {
@@ -210,8 +205,7 @@ public class DocumentService {
     }
 
     public Documents getParent(DocRef docRef, String type) throws Exception {
-        return (Documents) this.session.newRequest(GetDocumentParent).setInput(
-                docRef).set("type", type).execute();
+        return (Documents) this.session.newRequest(GetDocumentParent).setInput(docRef).set("type", type).execute();
     }
 
     public Documents query(String query) throws Exception {
@@ -230,13 +224,12 @@ public class DocumentService {
     }
 
     public Document addPermission(DocRef doc, String user, String permission, String acl, boolean blockInheritance) throws Exception {
-        OperationRequest req = this.session.newRequest(AddPermission).setInput(doc).set("user", user).set("permission", permission).set("blockInheritance",
+        OperationRequest req = this.session.newRequest(AddPermission).setInput(doc).set("username", user).set("permission", permission).set("blockInheritance",
                 blockInheritance);
         return (Document) req.execute();
     }
 
-    public Document setPermission(DocRef doc, String user, String permission)
-            throws Exception {
+    public Document setPermission(DocRef doc, String user, String permission) throws Exception {
         return this.setPermission(doc, user, permission, null, true);
     }
 
@@ -262,13 +255,11 @@ public class DocumentService {
     }
 
     public Document removeAcl(DocRef doc, String acl) throws Exception {
-        return (Document) this.session.newRequest(RemoveAcl).setInput(doc).set(
-                "acl", acl).execute();
+        return (Document) this.session.newRequest(RemoveAcl).setInput(doc).set("acl", acl).execute();
     }
 
     public Document setState(DocRef doc, String state) throws Exception {
-        return (Document) this.session.newRequest(SetDocumentState).setInput(doc).set(
-                "value", state).execute();
+        return (Document) this.session.newRequest(SetDocumentState).setInput(doc).set("value", state).execute();
     }
 
     public Document lock(DocRef doc) throws Exception {
@@ -288,15 +279,12 @@ public class DocumentService {
     }
 
     // TODO: value Serializable?
-    public Document setProperty(DocRef doc, String key, String value)
-            throws Exception {
-        return (Document) this.session.newRequest(SetProperty).setInput(doc).set(
-                "xpath", key).set("value", value).execute();
+    public Document setProperty(DocRef doc, String key, String value) throws Exception {
+        return (Document) this.session.newRequest(SetProperty).setInput(doc).set("xpath", key).set("value", value).execute();
     }
 
     public Document removeProperty(DocRef doc, String key) throws Exception {
-        return (Document) this.session.newRequest(RemoveProperty).setInput(doc).set(
-                "xpath", key).execute();
+        return (Document) this.session.newRequest(RemoveProperty).setInput(doc).set("xpath", key).execute();
     }
 
     public Document update(DocRef doc, PropertyMap properties) throws Exception {
@@ -317,36 +305,28 @@ public class DocumentService {
         return this.publish(doc, section, true);
     }
 
-    public Document publish(DocRef doc, DocRef section, boolean override)
-            throws Exception {
-        return (Document) this.session.newRequest(PublishDocument).setInput(doc).set(
-                "target", section).set("override", override).execute();
+    public Document publish(DocRef doc, DocRef section, boolean override) throws Exception {
+        return (Document) this.session.newRequest(PublishDocument).setInput(doc).set("target", section).set("override", override).execute();
     }
 
-    public Document createRelation(DocRef subject, String predicate,
-            DocRef object) throws Exception {
-        return (Document) this.session.newRequest(CreateRelation).setInput(subject).set(
-                "object", object).set("predicate", predicate).execute();
+    public Document createRelation(DocRef subject, String predicate, DocRef object) throws Exception {
+        return (Document) this.session.newRequest(CreateRelation).setInput(subject).set("object", object).set("predicate", predicate).execute();
     }
 
-    public Documents getRelations(DocRef doc, String predicate)
-            throws Exception {
+    public Documents getRelations(DocRef doc, String predicate) throws Exception {
         return this.getRelations(doc, predicate, true);
     }
 
-    public Documents getRelations(DocRef doc, String predicate, boolean outgoing)
-            throws Exception {
-        return (Documents) this.session.newRequest(GetRelations).setInput(doc).set(
-                "predicate", predicate).set("outgoing", outgoing).execute();
+    public Documents getRelations(DocRef doc, String predicate, boolean outgoing) throws Exception {
+        return (Documents) this.session.newRequest(GetRelations).setInput(doc).set("predicate", predicate).set("outgoing", outgoing).execute();
     }
 
     /**
      * @since 5.5
      */
-    public Documents getRelations(DocRef doc, String predicate, boolean outgoing, String graphName)
-            throws Exception {
-        return (Documents) this.session.newRequest(GetRelations).setInput(doc).set(
-                "predicate", predicate).set("outgoing", outgoing).set("graphName", graphName).execute();
+    public Documents getRelations(DocRef doc, String predicate, boolean outgoing, String graphName) throws Exception {
+        return (Documents) this.session.newRequest(GetRelations).setInput(doc).set("predicate", predicate).set("outgoing", outgoing).set("graphName", graphName)
+                .execute();
     }
 
     public void setBlobs(DocRef doc, Blobs blobs) throws Exception {
@@ -369,8 +349,7 @@ public class DocumentService {
     }
 
     public void setBlob(DocRef doc, Blob blob, String xpath) throws Exception {
-        OperationRequest req = this.session.newRequest(SetBlob).setInput(blob).set(
-                "document", doc);
+        OperationRequest req = this.session.newRequest(SetBlob).setInput(blob).set("document", doc);
         if (xpath != null) {
             req.set("xpath", xpath);
         }
@@ -418,7 +397,7 @@ public class DocumentService {
     public Document createVersion(DocRef doc) throws Exception {
         return this.createVersion(doc, null);
     }
-    
+
     /**
      * Gets versions with common fetch schemas (dc, common, ...).
      * 
@@ -429,7 +408,7 @@ public class DocumentService {
     public Documents getVersions(DocRef doc) throws Exception {
         return getVersions(doc, null);
     }
-    
+
     /**
      * Gets versions with specified data in addition to default schemas (dc, common, ...).
      * 
@@ -440,7 +419,7 @@ public class DocumentService {
     public Documents getVersions(DocRef doc, String schemas) throws Exception {
         OperationRequest req = this.session.newRequest(GetVersions).setInput(doc);
         // Data to fetch
-        if(schemas != null){
+        if (schemas != null) {
             req.setHeader(Constants.HEADER_NX_SCHEMAS, schemas);
         }
         return (Documents) req.execute();
@@ -452,8 +431,7 @@ public class DocumentService {
      *
      * See {@link VersionIncrement}
      */
-    public Document createVersion(DocRef doc, String increment)
-            throws Exception {
+    public Document createVersion(DocRef doc, String increment) throws Exception {
         OperationRequest req = this.session.newRequest(CreateVersion).setInput(doc);
         if (increment != null) {
             req.set("increment", increment);
