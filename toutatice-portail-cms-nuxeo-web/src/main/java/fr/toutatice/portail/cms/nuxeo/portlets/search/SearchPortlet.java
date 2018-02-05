c164c6b1ca00085fcf78394b6363d37aceb64efc
no_author <contact@osivia.com>
2018-02-05 09:08:00 +0000

    Merge branche 4.4-patchsecu.

--- toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/portlets/search/SearchPortlet.java
+++ toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/portlets/search/SearchPortlet.java
@@ -29,6 +29,7 @@ import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.portlet.WindowState;
 
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
 import org.jboss.portal.core.model.portal.Page;
@@ -117,7 +118,8 @@ public class SearchPortlet extends CMSPortlet {
             // View
             if ("search".equals(action)) {
                 // Refresh search keywords
-                response.setRenderParameter("keywords", request.getParameter("keywords"));
+                String keywords = request.getParameter("keywords");
+                response.setRenderParameter("keywords", keywords);
             }
         } else if ("admin".equals(request.getPortletMode().toString())) {
             // Admin
@@ -212,7 +214,7 @@ public class SearchPortlet extends CMSPortlet {
                 int minPage = Math.max(0, currentPage - docs.getPageSize());
                 int maxPage = Math.min(currentPage + docs.getPageSize(), docs.getPageCount()) - 1;
 
-                request.setAttribute("keywords", keywords);
+                request.setAttribute("keywords", StringEscapeUtils.escapeHtml(keywords));
                 request.setAttribute("documents", documentsDTO);
                 request.setAttribute("totalSize", docs.getTotalSize());
                 request.setAttribute("currentPage", currentPage);
