c9f840ae33de3132ada978d8e40ebef4518dcdef
no_author <contact@osivia.com>
2018-02-06 14:54:19 +0000

    Déplacement des salles et gestion des droits lors du changement d'espace (fixes #1648).

--- toutatice-portail-cms-nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/adapters/DocumentSecurityService.java
+++ toutatice-portail-cms-nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/adapters/DocumentSecurityService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.nuxeo.ecm.automation.client.OperationRequest;
 import org.nuxeo.ecm.automation.client.Session;
+import org.nuxeo.ecm.automation.client.model.DocRef;
 import org.nuxeo.ecm.automation.client.model.Document;
 import org.nuxeo.ecm.automation.client.model.DocumentPermissions;
 import org.nuxeo.ecm.automation.client.model.ListString;
@@ -81,7 +82,7 @@ public class DocumentSecurityService {
      * - if removeAll is true, removes all ACEs of given ACL
      * - if userNames is set, removes all ACEs of given users.
      * 
-     * @param document
+     * @param docRef
      * @param permissions
      * @param userNames
      * @param acl
@@ -90,14 +91,15 @@ public class DocumentSecurityService {
      * @return document
      * @throws Exception
      */
-    public Document removePermissions (Document document, DocumentPermissions permissions, List<String> userNames, String acl, boolean removeAll, boolean blockInheritance) throws Exception {
+    public Document removePermissions(DocRef docRef, DocumentPermissions permissions, List<String> userNames, String acl, boolean removeAll,
+            boolean blockInheritance) throws Exception {
         // Coherence check
         if(blockInheritance && INHERITED_ACL.equalsIgnoreCase(acl)){
             throw new Exception("You can not remove permissions in inherited ACL if you block inheritance");
         }
         
         OperationRequest request = getSession().newRequest(REMOVE_PERMISSIONS_OP)
-            .setInput(document)
+                .setInput(docRef)
             .set("acl", acl)
             .set("aces", permissions)
             .set("userNames", ListString.getInstance().getAsString(userNames))
@@ -110,14 +112,14 @@ public class DocumentSecurityService {
      * Remove given ACL on document,
      * i.e. inherited or local ACL.
      * 
-     * @param document
+     * @param docRef
      * @param acl
      * @return document
      * @throws Exception
      */
-    public Document removeGroupPermissions(Document document, String acl) throws Exception{
+    public Document removeGroupPermissions(DocRef docRef, String acl) throws Exception {
         OperationRequest request = getSession().newRequest(REMOVE_PERMISSIONS_OP)
-                .setInput(document)
+                .setInput(docRef)
                 .set("acl", acl);
         return (Document) request.execute();
     }
