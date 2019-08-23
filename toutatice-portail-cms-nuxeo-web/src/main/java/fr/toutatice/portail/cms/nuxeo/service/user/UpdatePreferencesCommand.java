be4b3e2591176d20b5aa0654e51b8186bfabed92
Cédric Krommenhoek <cedric.krommenhoek@gmail.com>
2019-08-23 10:38:34 +0200

    :sparkles: User saved searches.

--- toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/service/user/UpdatePreferencesCommand.java
+++ toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/service/user/UpdatePreferencesCommand.java
@@ -1,5 +1,5 @@
 /*
-* (C) Copyright 2018  Académie de Rennes (http://www.ac-rennes.fr/), OSIVIA (http://www.osivia.com) and others.
+ * (C) Copyright 2018  Académie de Rennes (http://www.ac-rennes.fr/), OSIVIA (http://www.osivia.com) and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -13,46 +13,76 @@
  */
 package fr.toutatice.portail.cms.nuxeo.service.user;
 
-import java.util.Map;
-
-import org.nuxeo.ecm.automation.client.Session;
-import org.nuxeo.ecm.automation.client.adapters.DocumentService;
-import org.nuxeo.ecm.automation.client.model.IdRef;
-import org.nuxeo.ecm.automation.client.model.PropertyMap;
-import org.osivia.portal.api.user.UserPreferences;
-
 import fr.toutatice.portail.cms.nuxeo.api.INuxeoCommand;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import org.apache.commons.collections.CollectionUtils;
+import org.nuxeo.ecm.automation.client.Session;
+import org.nuxeo.ecm.automation.client.adapters.DocumentService;
+import org.nuxeo.ecm.automation.client.model.DocRef;
+import org.nuxeo.ecm.automation.client.model.IdRef;
+import org.nuxeo.ecm.automation.client.model.PropertyMap;
+import org.osivia.portal.api.user.UserPreferences;
+import org.osivia.portal.api.user.UserSavedSearch;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Command to store user preferences in UserProfile document
+ *
  * @author Loïc Billon
+ * @author Cédric Krommenhoek
  * @see INuxeoCommand
  */
 public class UpdatePreferencesCommand implements INuxeoCommand {
 
-	/**
-	 * 
-	 */
-	public static final String METADATA_FOLDERS_PREFS = "ttc_userprofile:folders_prefs";
-	
+    /**
+     * Folder preferences xpath.
+     */
+    public static final String METADATA_FOLDERS_PREFS = "ttc_userprofile:folders_prefs";
+    /**
+     * Folder identifier.
+     */
+    public static final String METADATA_FOLDERS_PREFS_ID = "webid";
+    /**
+     * Folder display value.
+     */
+    public static final String METADATA_FOLDERS_PREFS_VALUE = "display_style";
 
-	/**
-	 * 
-	 */
-	public static final String METADATA_FOLDERS_PREFS_ID = "webid";
-	
+    /**
+     * Saved searches xpath.
+     */
+    public static final String SAVED_SEARCHES_XPATH = "ttc_userprofile:savedSearches";
+    /**
+     * Saved search identifier.
+     */
+    public static final String SAVED_SEARCH_ID = "searchId";
+    /**
+     * Saved search display name.
+     */
+    public static final String SAVED_SEARCH_DISPLAY_NAME = "displayName";
+    /**
+     * Saved search order.
+     */
+    public static final String SAVED_SEARCH_ORDER = "order";
+    /**
+     * Saved search data.
+     */
+    public static final String SAVED_SEARCH_DATA = "data";
 
-	/**
-	 * 
-	 */
-	public static final String METADATA_FOLDERS_PREFS_VALUE = "display_style";
-	
-    /** Space statistics. */
+
+    /**
+     * User preferences.
+     */
     private final UserPreferences preferences;
 
 
+    /**
+     * Constructor.
+     *
+     * @param preferences user preferences
+     */
     public UpdatePreferencesCommand(UserPreferences preferences) {
         super();
         this.preferences = preferences;
@@ -64,45 +94,76 @@ public class UpdatePreferencesCommand implements INuxeoCommand {
      */
     @Override
     public Object execute(Session nuxeoSession) throws Exception {
-
         // Document service
         DocumentService documentService = nuxeoSession.getAdapter(DocumentService.class);
 
         PropertyMap properties = new PropertyMap();
-        properties.set(METADATA_FOLDERS_PREFS,this.convertFolderPrefsJson(preferences.getFolderDisplays()));
+        properties.set(METADATA_FOLDERS_PREFS, this.convertFolders(this.preferences.getFolderDisplays()));
+        properties.set(SAVED_SEARCHES_XPATH, this.convertSavedSearches(this.preferences.getSavedSearches()));
 
-        IdRef docRef = new IdRef(preferences.getDocId());
-        documentService.update(docRef, properties);
-    	
-        return null;
+        DocRef docRef = new IdRef(this.preferences.getDocId());
+
+        return documentService.update(docRef, properties);
     }
 
 
     /**
-	 * @param folderDisplays
-	 * @return
-	 */
-	private String convertFolderPrefsJson(Map<String, String> folderDisplays) {
+     * Convert folders to JSON.
+     *
+     * @param folders folders
+     * @return JSON
+     */
+    private String convertFolders(Map<String, String> folders) {
+        JSONArray array = new JSONArray();
 
-		JSONArray array = new JSONArray();
-		
-		for(Map.Entry<String, String> entry : folderDisplays.entrySet()) {
-			JSONObject object = new JSONObject();
-			object.put(METADATA_FOLDERS_PREFS_ID, entry.getKey());
-			object.put(METADATA_FOLDERS_PREFS_VALUE, entry.getValue());
-			array.add(object);
-			
-		}
-		
-		return array.toString();
-	}
+        for (Map.Entry<String, String> entry : folders.entrySet()) {
+            JSONObject object = new JSONObject();
+            object.put(METADATA_FOLDERS_PREFS_ID, entry.getKey());
+            object.put(METADATA_FOLDERS_PREFS_VALUE, entry.getValue());
 
-	/**
+            array.add(object);
+        }
+
+        return array.toString();
+    }
+
+
+    /**
+     * Convert saved searches to JSON.
+     *
+     * @param savedSearches saved searches
+     * @return JSON
+     */
+    private String convertSavedSearches(List<UserSavedSearch> savedSearches) {
+        String result;
+
+        if (CollectionUtils.isEmpty(savedSearches)) {
+            result = null;
+        } else {
+            JSONArray array = new JSONArray();
+
+            for (UserSavedSearch savedSearch : savedSearches) {
+                JSONObject object = new JSONObject();
+                object.put(SAVED_SEARCH_ID, savedSearch.getId());
+                object.put(SAVED_SEARCH_DISPLAY_NAME, savedSearch.getDisplayName());
+                object.put(SAVED_SEARCH_ORDER, savedSearch.getOrder());
+                object.put(SAVED_SEARCH_DATA, savedSearch.getData());
+
+                array.add(object);
+            }
+
+            result = array.toString();
+        }
+
+        return result;
+    }
+
+
+    /**
      * {@inheritDoc}
      */
     @Override
     public String getId() {
-
         return null;
     }
 
--- toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/service/user/UserPreferencesDelegation.java
+++ toutatice-portail-cms-nuxeo-web/src/main/java/fr/toutatice/portail/cms/nuxeo/service/user/UserPreferencesDelegation.java
@@ -1,5 +1,5 @@
 /*
-* (C) Copyright 2018  Académie de Rennes (http://www.ac-rennes.fr/), OSIVIA (http://www.osivia.com) and others.
+ * (C) Copyright 2018  Académie de Rennes (http://www.ac-rennes.fr/), OSIVIA (http://www.osivia.com) and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -13,12 +13,11 @@
  */
 package fr.toutatice.portail.cms.nuxeo.service.user;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.portlet.PortletContext;
-import javax.servlet.http.HttpSession;
-
+import fr.toutatice.portail.cms.nuxeo.api.INuxeoCommand;
+import fr.toutatice.portail.cms.nuxeo.api.NuxeoController;
+import fr.toutatice.portail.cms.nuxeo.api.services.NuxeoCommandContext;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
 import org.nuxeo.ecm.automation.client.model.Document;
 import org.nuxeo.ecm.automation.client.model.PropertyList;
 import org.nuxeo.ecm.automation.client.model.PropertyMap;
@@ -29,129 +28,175 @@ import org.osivia.portal.api.directory.v2.DirServiceFactory;
 import org.osivia.portal.api.directory.v2.model.Person;
 import org.osivia.portal.api.directory.v2.service.PersonService;
 import org.osivia.portal.api.user.UserPreferences;
+import org.osivia.portal.api.user.UserSavedSearch;
 import org.osivia.portal.core.cms.CMSServiceCtx;
 
-import fr.toutatice.portail.cms.nuxeo.api.INuxeoCommand;
-import fr.toutatice.portail.cms.nuxeo.api.NuxeoController;
-import fr.toutatice.portail.cms.nuxeo.api.services.NuxeoCommandContext;
+import javax.portlet.PortletContext;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- * @author Loïc Billon
+ * User preferences delegation.
  *
+ * @author Loïc Billon
+ * @author Cédric Krommenhoek
  */
 public class UserPreferencesDelegation {
 
-		
-	private static final String PREFS_SESSION_ATTRIBUTE = "osivia.user.preferences";
-	private PersonService personService;
+    /**
+     * Session attribute.
+     */
+    private static final String SESSION_ATTRIBUTE = "osivia.user.preferences";
 
-	public UserPreferencesDelegation() {
-		personService = DirServiceFactory.getService(PersonService.class);
-	}
-	
-	/**
-	 * @param cmsContext
-	 * @return
-	 * @throws PortalException 
-	 */
-	public UserPreferences getPreferences(PortalControllerContext context) throws PortalException {
-		
-		// Get session attribute
-		HttpSession httpSession = context.getHttpServletRequest().getSession();
+
+    /**
+     * Person service.
+     */
+    private final PersonService personService;
+
+
+    /**
+     * Constructor.
+     */
+    public UserPreferencesDelegation() {
+        super();
+        this.personService = DirServiceFactory.getService(PersonService.class);
+    }
+
+
+    /**
+     * Get user preferences.
+     *
+     * @param portalControllerContext portal controller context
+     * @return user preferences
+     */
+    public UserPreferences getPreferences(PortalControllerContext portalControllerContext) throws PortalException {
+        // Session
+        HttpSession httpSession = portalControllerContext.getHttpServletRequest().getSession();
         // Session attribute
-        Object attribute = httpSession.getAttribute(PREFS_SESSION_ATTRIBUTE);
+        Object attribute = httpSession.getAttribute(SESSION_ATTRIBUTE);
 
         // User session
         UserPreferences userPreferences = null;
         if ((attribute == null) || !(attribute instanceof UserPreferences)) {
-        	
-        	// If not loaded, get it in nuxeo
-        	if(httpSession.getAttribute("PRINCIPAL_TOKEN") != null) {
-	        	String user = (String) httpSession.getAttribute("PRINCIPAL_TOKEN");
-	
-	        	Person person = personService.getPerson(user);
-	        	
-	        	if(person != null) {
-	        	
-	        		Object ecmProfile = personService.getEcmProfile(context, person);
-	        		
-	        		if(ecmProfile instanceof Document) {
-	        			Document profile = (Document) ecmProfile;
-	        			
-	        			userPreferences = toPreferencesDto(profile);
-	        		}
-	                httpSession.setAttribute(PREFS_SESSION_ATTRIBUTE, userPreferences);
-	
-	        	}
-        	}
-        	
+            // If not loaded, get it in nuxeo
+            if (httpSession.getAttribute("PRINCIPAL_TOKEN") != null) {
+                String user = (String) httpSession.getAttribute("PRINCIPAL_TOKEN");
+
+                Person person = personService.getPerson(user);
+
+                if (person != null) {
+                    Object ecmProfile = personService.getEcmProfile(portalControllerContext, person);
+
+                    if (ecmProfile instanceof Document) {
+                        Document profile = (Document) ecmProfile;
+
+                        userPreferences = this.toPreferencesDto(profile);
+                    }
+
+                    httpSession.setAttribute(SESSION_ATTRIBUTE, userPreferences);
+                }
+            }
         } else {
             userPreferences = (UserPreferences) attribute;
         }
 
         return userPreferences;
-		
-	}
-	
-	/**
-	 * Update method
-	 * @param cmsContext context
-	 * @param httpSession the user session
-	 */
-	public void updateUserPreferences(CMSServiceCtx cmsContext, HttpSession httpSession) {
-		
-        Object attribute = httpSession.getAttribute(PREFS_SESSION_ATTRIBUTE);
-        
+    }
+
+
+    /**
+     * Update method.
+     *
+     * @param cmsContext  CMS context
+     * @param httpSession session
+     */
+    public void updateUserPreferences(CMSServiceCtx cmsContext, HttpSession httpSession) {
+        Object attribute = httpSession.getAttribute(SESSION_ATTRIBUTE);
+
         if ((attribute != null) && attribute instanceof UserPreferences) {
-        	UserPreferences userPreferences = (UserPreferences) attribute;
-        	
-        	if(userPreferences.isUpdate()) {
-	            // Portlet context
-	            PortletContext portletContext = cmsContext.getPortletCtx();
-        		
-	            // Nuxeo controller
-	            NuxeoController nuxeoController = new NuxeoController(portletContext);
-	            nuxeoController.setAuthType(NuxeoCommandContext.AUTH_TYPE_SUPERUSER);
-	            nuxeoController.setCacheType(CacheInfo.CACHE_SCOPE_NONE);
-	
-	            // Nuxeo command
-	            INuxeoCommand command = new UpdatePreferencesCommand(userPreferences);
-	            nuxeoController.executeNuxeoCommand(command);
-        	}
-        	
+            UserPreferences userPreferences = (UserPreferences) attribute;
+
+            if (userPreferences.isUpdate()) {
+                // Portlet context
+                PortletContext portletContext = cmsContext.getPortletCtx();
+
+                // Nuxeo controller
+                NuxeoController nuxeoController = new NuxeoController(portletContext);
+                nuxeoController.setAuthType(NuxeoCommandContext.AUTH_TYPE_SUPERUSER);
+                nuxeoController.setCacheType(CacheInfo.CACHE_SCOPE_NONE);
+
+                // Nuxeo command
+                INuxeoCommand command = new UpdatePreferencesCommand(userPreferences);
+                nuxeoController.executeNuxeoCommand(command);
+            }
+
         }
-	}
+    }
 
 
-	/**
+    /**
      * Convert from UserProfile document to UserPreferences pojo.
-     * 
+     *
      * @param profile profile Nuxeo document
      * @return user preferences
      */
-	private UserPreferences toPreferencesDto(Document profile) {
-		Map<String, String> prefs = new HashMap<>();
+    private UserPreferences toPreferencesDto(Document profile) {
+        // User preferences
+        UserPreferences userPreferences = new UserPreferences(profile.getId());
 
-		PropertyList list = profile.getProperties().getList(UpdatePreferencesCommand.METADATA_FOLDERS_PREFS);
+        // Folder preferences
+        Map<String, String> folders = new HashMap<>();
+        PropertyList foldersPropertyList = profile.getProperties().getList(UpdatePreferencesCommand.METADATA_FOLDERS_PREFS);
+        if ((foldersPropertyList != null) && !foldersPropertyList.isEmpty()) {
+            for (int i = 0; i < foldersPropertyList.size(); i++) {
+                PropertyMap folderPropertyMap = foldersPropertyList.getMap(i);
 
-		if ((list != null) && !list.isEmpty()) {
-            for (int i = 0; i < list.size(); i++) {
-                PropertyMap map = list.getMap(i);
-
-                Object objectValue = map.get(UpdatePreferencesCommand.METADATA_FOLDERS_PREFS_VALUE);
-                Object objectId = map.get(UpdatePreferencesCommand.METADATA_FOLDERS_PREFS_ID);
+                Object objectValue = folderPropertyMap.get(UpdatePreferencesCommand.METADATA_FOLDERS_PREFS_VALUE);
+                Object objectId = folderPropertyMap.get(UpdatePreferencesCommand.METADATA_FOLDERS_PREFS_ID);
 
                 if (objectId != null && objectValue != null) {
-                    prefs.put(objectId.toString(), objectValue.toString());
+                    folders.put(objectId.toString(), objectValue.toString());
                 }
             }
-		}
+        }
+        userPreferences.setFolderDisplays(folders);
 
-		UserPreferences upf = new UserPreferences(profile.getId());
-		upf.setFolderDisplays(prefs);
+        // Saved searches
+        List<UserSavedSearch> savedSearches;
+        PropertyList savedSearchesPropertyList = profile.getProperties().getList(UpdatePreferencesCommand.SAVED_SEARCHES_XPATH);
+        if ((savedSearchesPropertyList == null) || savedSearchesPropertyList.isEmpty()) {
+            savedSearches = null;
+        } else {
+            savedSearches = new ArrayList<>(savedSearchesPropertyList.size());
 
-		return upf;
-	}
+            for (int i = 0; i < savedSearchesPropertyList.size(); i++) {
+                PropertyMap savedSearchPropertyMap = savedSearchesPropertyList.getMap(i);
+
+                // Identifier
+                int id = NumberUtils.toInt(savedSearchPropertyMap.getString(UpdatePreferencesCommand.SAVED_SEARCH_ID));
+                // Saved search
+                UserSavedSearch savedSearch = new UserSavedSearch(id);
+                // Display name
+                String displayName = savedSearchPropertyMap.getString(UpdatePreferencesCommand.SAVED_SEARCH_DISPLAY_NAME);
+                savedSearch.setDisplayName(displayName);
+                // Order
+                Integer order = NumberUtils.createInteger(savedSearchPropertyMap.getString(UpdatePreferencesCommand.SAVED_SEARCH_ORDER));
+                savedSearch.setOrder(order);
+                // Data
+                String data = savedSearchPropertyMap.getString(UpdatePreferencesCommand.SAVED_SEARCH_DATA);
+                savedSearch.setData(data);
+
+                savedSearches.add(savedSearch);
+            }
+        }
+        userPreferences.setSavedSearches(savedSearches);
+
+        return userPreferences;
+    }
 
 }
 
