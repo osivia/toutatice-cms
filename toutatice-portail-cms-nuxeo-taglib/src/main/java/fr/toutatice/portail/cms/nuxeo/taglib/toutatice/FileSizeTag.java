cd370fbe0086c08974fd448420892b436bde3ff9
Cédric Krommenhoek <cedric.krommenhoek@gmail.com>
2021-12-08 13:18:19 +0100

    ✨ Add escape HTML option to file size tag

--- toutatice-portail-cms-nuxeo-taglib/src/main/java/fr/toutatice/portail/cms/nuxeo/taglib/toutatice/FileSizeTag.java
+++ toutatice-portail-cms-nuxeo-taglib/src/main/java/fr/toutatice/portail/cms/nuxeo/taglib/toutatice/FileSizeTag.java
@@ -1,23 +1,23 @@
 package fr.toutatice.portail.cms.nuxeo.taglib.toutatice;
 
-import java.io.IOException;
-import java.text.NumberFormat;
-import java.util.Locale;
-
-import javax.servlet.ServletRequest;
-import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.JspWriter;
-import javax.servlet.jsp.PageContext;
-
+import fr.toutatice.portail.cms.nuxeo.api.NuxeoController;
+import fr.toutatice.portail.cms.nuxeo.api.domain.DocumentDTO;
+import fr.toutatice.portail.cms.nuxeo.taglib.common.ToutaticeSimpleTag;
+import org.apache.commons.lang.BooleanUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.taglibs.standard.tag.common.core.OutSupport;
 import org.osivia.portal.api.html.HtmlFormatter;
 import org.osivia.portal.api.internationalization.Bundle;
 import org.osivia.portal.api.internationalization.IBundleFactory;
 import org.osivia.portal.api.internationalization.IInternationalizationService;
 import org.osivia.portal.api.locator.Locator;
 
-import fr.toutatice.portail.cms.nuxeo.api.NuxeoController;
-import fr.toutatice.portail.cms.nuxeo.api.domain.DocumentDTO;
-import fr.toutatice.portail.cms.nuxeo.taglib.common.ToutaticeSimpleTag;
+import javax.servlet.ServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import java.io.IOException;
+import java.util.Locale;
 
 /**
  * Format file size tag.
@@ -27,17 +27,20 @@ import fr.toutatice.portail.cms.nuxeo.taglib.common.ToutaticeSimpleTag;
  */
 public class FileSizeTag extends ToutaticeSimpleTag {
 
-    /** File size units. */
-    private static final String[] UNITS = {"BYTE", "KILOBYTE", "MEGABYTE", "GIGABYTE", "TERABYTE"};
-    /** Unit factor. */
-    private static final double UNIT_FACTOR = 1024;
-
-
-    /** File size. */
-    private Long size;
-
-    /** Bundle factory. */
+    /**
+     * Bundle factory.
+     */
     private final IBundleFactory bundleFactory;
+
+
+    /**
+     * File size.
+     */
+    private Long size;
+    /**
+     * Escape HTML indicator.
+     */
+    private Boolean escapeHtml;
 
 
     /**
@@ -67,20 +70,21 @@ public class FileSizeTag extends ToutaticeSimpleTag {
         // Bundle
         Bundle bundle = this.bundleFactory.getBundle(locale);
 
+        String text = HtmlFormatter.formatSize(locale, bundle, size);
+        if (BooleanUtils.isFalse(this.escapeHtml)) {
+            text = StringUtils.replace(text, "&nbsp;", "\u202f");
+        }
+
         JspWriter out = pageContext.getOut();
+        out.write(text);
+    }
 
 
-        out.write(HtmlFormatter.formatSize(locale, bundle, size));
-     }
-
-
-    /**
-     * Setter for size.
-     *
-     * @param size the size to set
-     */
     public void setSize(Long size) {
         this.size = size;
     }
 
+    public void setEscapeHtml(Boolean escapeHtml) {
+        this.escapeHtml = escapeHtml;
+    }
 }
